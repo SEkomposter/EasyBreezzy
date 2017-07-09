794c7b06d911974455c2c5782a3e1bc1be68d807
se.komposterr <se.komposterr@gmail.com>
2017-07-09 13:39:51 +0300

    next update 09.07.2017 13:41

---
+++ src/Util/HibernateUtil.java
@@ -0,0 +1,12 @@
+package Util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory = null;
+
+           Configuration conf = new Configuration().configure();
+
+}
